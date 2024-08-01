bb922e5b69bb0c04a70fcc64352da6ed9b88b44a
“Mohammad_janoudi” <00319408@rijnijssel.nl>
2024-08-02 00:54:44 +0200

    mybuddypush

--- php/login.php
+++ php/login.php
@@ -1,50 +1,53 @@
 <?php
 session_start();
 
-if (isset($_POST['email']) && isset($_POST['password'])) {
 require '../private/conn.php';
-    $email = $_POST['email'];
+
+if (isset($_POST['gebruiker_naam']) && isset($_POST['password'])) {
+    $username = $_POST['gebruiker_naam'];
     $password = $_POST['password'];
 
-  
+    $sql = 'SELECT id, Wachtwoord, usertype FROM users WHERE gebruiker_naam = :gebruiker_naam';
+    $sth = $dbh->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+    $sth->execute(array(':gebruiker_naam' => $username));
 
     $matchingUsers = $sth->fetchAll(PDO::FETCH_ASSOC);
 
     if (count($matchingUsers) > 0) {
         foreach ($matchingUsers as $rsUser) {
             if ($password == $rsUser['Wachtwoord']) {
-                $_SESSION['id'] = $rsUser['id']; 
+                $_SESSION['user_id'] = $rsUser['id']; 
 
                 if (isset($rsUser['usertype'])) {
                     if ($rsUser['usertype'] == 'admin') {
-                        $_SESSION['user'] = 'Admin'; 
+                        $_SESSION['user'] = 'admin'; 
 
                         header('location: ../index.php?page=Admin_shop');
                         exit();
                     } else if ($rsUser['usertype'] == 'user') {
                         $_SESSION['user'] = 'gebruiker';
-                        header('location: ../index.php?page=Groepen');
+                        header('location: ../index.php?page=shop');
                         exit();
                     }
                 } else {
-                    $_SESSION['melding'] = 'User type not found';
-                    header('location: ../index.php?page=Home');
+                    $_SESSION['medling'] = 'User type not found';
+                    header('location: ../index.php?page=login');
                     exit();
                 }
             }
         }
-
-        $_SESSION['melding'] = 'Wrong password';
-        header('location: ../index.php?page=Home');
+        
+        $_SESSION['medling'] = 'Wrong password';
+        header('location: ../index.php?page=login');
         exit();
     } else {
-        $_SESSION['melding'] = 'Incorrect username';
-        header('location: ../index.php?page=Home');
+        $_SESSION['medling'] = 'Incorrect username';
+        header('location: ../index.php?page=login');
         exit();
     }
 } else {
-    $_SESSION['melding'] = 'Username and password are required.';
-    header('location: ../index.php?page=Home');
+    $_SESSION['medling'] = 'Username and password are required.';
+    header('location: ../index.php?page=login');
     exit();
 }
-?>  
+?>
